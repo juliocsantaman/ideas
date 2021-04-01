c8f2dbc8c3d7a8c430cf642ce8d5ca6878b0fe87
juliocsantaman <juliocsantaman@gmail.com>
2021-04-01 13:45:32 -0500

    I added icons, edit and delete.

--- js/index.js
+++ js/index.js
@@ -6,15 +6,24 @@ btnAdd.addEventListener("click",  addIdea);
 
 function addIdea() {
     const p = document.createElement("p");
-    const i = document.createElement("i");
+    const iEdit = document.createElement("i");
+    const iDelete = document.createElement("i");
+    const spanIcons = document.createElement("span");
 
     p.innerHTML = textarea.value;
 
-    i.classList.add("fas", "fa-edit");
+    iEdit.classList.add("fas", "fa-edit");
+    iDelete.classList.add("fas", "fa-trash-alt");
+    spanIcons.appendChild(iEdit);
+    spanIcons.appendChild(iDelete);
 
     // <i class="fas fa-edit"></i>
+    // <i class="fas fa-trash-alt"></i>
 
-    p.appendChild(i);
+    // p.appendChild(iEdit);
+    // p.appendChild(iDelete);
+
+    p.appendChild(spanIcons);
 
     p.classList.add("text-info", "bg-dark", "p-3");
 
