00e1609b10dd6a32d6b17521371285be1d477b91
juliocsantaman <juliocsantaman@gmail.com>
2021-04-01 21:53:50 -0500

    I did two functionalities, edit and delete the ideas.

--- js/index.js
+++ js/index.js
@@ -2,34 +2,64 @@ const textarea = document.getElementById("textarea");
 const btnAdd = document.getElementById("btn-add");
 const ideaContainer = document.getElementById("idea-container");
 
-btnAdd.addEventListener("click",  addIdea);
+btnAdd.addEventListener("click", addIdea);
 
 function addIdea() {
-    const p = document.createElement("p");
-    const iEdit = document.createElement("i");
-    const iDelete = document.createElement("i");
-    const spanIcons = document.createElement("span");
 
-    p.innerHTML = textarea.value;
+    if(textarea.value != "") {
+        const p = document.createElement("p");
+        const iEdit = document.createElement("i");
+        const iDelete = document.createElement("i");
+        const spanIcons = document.createElement("span");
+    
+        p.innerHTML = textarea.value;
+    
+        iEdit.classList.add("fas", "fa-edit");
+        iDelete.classList.add("fas", "fa-trash-alt");
+        spanIcons.appendChild(iEdit);
+        spanIcons.appendChild(iDelete);
+    
+        p.appendChild(spanIcons);
+    
+        p.classList.add("text-info", "bg-dark", "p-3");
+    
+        ideaContainer.appendChild(p);
+    
+        textarea.value = "";
+    
+        iEdit.addEventListener("click", () => {
+            //Create a textarea.
+            let textarea = document.createElement('textarea');
+            //Replace current node with textarea.
+            p.replaceWith(textarea);
+            textarea.focus();
+    
+            //If textarea lose focus so adding new task or leaving current task.
+            textarea.addEventListener('blur', (ev) => {
+    
+                if (textarea.value != '') {
+                    p.innerHTML = `${textarea.value}`;
+                    textarea.replaceWith(p);
+                    p.appendChild(spanIcons);
+                } else {
+    
+                    textarea.replaceWith(p);
+                }
+    
+            });
+        });
+        iDelete.addEventListener("click", deleteIdea);
 
-    iEdit.classList.add("fas", "fa-edit");
-    iDelete.classList.add("fas", "fa-trash-alt");
-    spanIcons.appendChild(iEdit);
-    spanIcons.appendChild(iDelete);
+    } else {
+        alert("No puedes dejar el campo vacío. :/");
+    }
 
-    // <i class="fas fa-edit"></i>
-    // <i class="fas fa-trash-alt"></i>
-
-    // p.appendChild(iEdit);
-    // p.appendChild(iDelete);
-
-    p.appendChild(spanIcons);
-
-    p.classList.add("text-info", "bg-dark", "p-3");
-
-    ideaContainer.appendChild(p);
-
-    textarea.value = "";
 
 }
+
+function deleteIdea(iDelete) {
+    iDelete.explicitOriginalTarget.parentNode.parentNode.remove();
+}
+
+
 
